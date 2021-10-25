77dae3ecdddcb11ab47f2441618239938c809fa4
Mohamed Salman <salman.primary@gmail.com>
2021-10-25 15:02:34 +0530

    Add project files.

---
+++ PokitPal.RaizCards.Client/wwwroot/js/helper.js
@@ -0,0 +1,8 @@
+﻿window.ScrollHorizontal = (elmId) => {
+    var scrollContainer  = document.getElementById(elmId);
+
+    scrollContainer.addEventListener("wheel", (evt) => {
+        evt.preventDefault();
+        scrollContainer.scrollLeft += evt.deltaY;
+    });
+}
